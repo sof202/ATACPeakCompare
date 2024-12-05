157233ef04b64f4b6888c31d59b43e6c8e08db18
sof202 <s.o.fletcher@exeter.ac.uk>
2024-12-05 16:16:16 +0000

    feat: add functions to label peak status of bases from narrow peaks

---
+++ Python_Scripts/label_peak_type.py
@@ -0,0 +1,72 @@
+from extract_region import extract_bedbase_region
+import pandas as pd
+from typing import Optional
+
+
+def convert_narrow_peak_to_bedbase(peak_data: pd.DataFrame,
+                                   chromosome: str,
+                                   start: int,
+                                   end: int) -> pd.DataFrame:
+    """Converts peaks in a selected region of narrow peak data into bedbase
+    format
+
+    Args
+        peak_data (pandas.DataFrame): Narrow peak data in BED3+7 format.
+        chromosome (str): Chromosome to extract.
+        start (int): Start of region.
+        end (int): End of region.
+
+    Returns:
+        A pandas.DataFrame in bedbase format of the selected region where the
+        score column is 0 if no peak is at that base, and 1 if there is a peak.
+    """
+    peak_data = extract_bedbase_region(peak_data, chromosome, start, end)
+
+    # extract_bedbase_region() will return NaN values in the SCORE column for
+    # all bases that do not exist in the narrow peak file. This is exactly the
+    # bases that are not within a peak. A trick here is to convert to Boolean
+    # values to convert all non-NaN scores into 1 and all NaN scores to 0.
+    peak_data["SCORE"] = peak_data["SCORE"].fillna(0).astype(bool).astype(int)
+    return peak_data
+
+
+def label_peak_type(unmerged_peaks: pd.DataFrame,
+                    merged_peaks: pd.DataFrame) -> Optional[pd.DataFrame]:
+    """Using two bedbase files from narrow peak files the types of peak are
+    determined.
+
+    Args:
+        unmerged_peaks (pandas DataFrame): A set of peaks before merging peaks
+        in bedbase format.
+        merged_peaks (pandas DataFrame): A set of peaks after merging peaks in
+        bedbase format.
+
+    Returns:
+        A pandas DataFrame in bedbase format with a score column indicating the
+        peak type: 0 indicates not a peak, 1 indicates a peak before merging,
+        2 indicates a peak only after mering. Returns None if the input
+        data frames do not align (different bases).
+    """
+    if unmerged_peaks["BASE"] != merged_peaks["BASE"]:
+        print("ERROR: unmerged and merged peak files have been created over",
+              "different regions.")
+        return None
+    if unmerged_peaks["CHR"] != merged_peaks["CHR"]:
+        print("ERROR: unmerged and merged peak files have been created over",
+              "different chromosomes.")
+        return None
+    labelled_peaks = pd.merge(
+        unmerged_peaks,
+        merged_peaks,
+        on=["CHR", "BASE"],
+        how="inner"
+    )
+
+    # Note: Because MACS deletes small peaks AFTER merging, the peaks in the
+    # merged peaks file is necessarily a superset of the peaks in the unmerged
+    # peaks file.
+    labelled_peaks["SCORE"] = labelled_peaks["SCORE_x"] + \
+        labelled_peaks["SCORE_y"]
+    labelled_peaks = labelled_peaks.loc[["CHR", "BASE", "SCORE"]]
+
+    return labelled_peaks
