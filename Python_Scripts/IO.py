4639a801d254c9003de0f21e473f9851b6e1d8df
sof202 <s.o.fletcher@exeter.ac.uk>
2024-12-06 14:04:25 +0000

    refactor: put all file types into classes

--- Python_Scripts/IO.py
+++ Python_Scripts/IO.py
@@ -2,99 +2,199 @@ import pandas as pd
 from typing import Optional
 
 
-def read_bedgraph(file_path: str) -> Optional[pd.DataFrame]:
-    """Reads a bedgraph file into a pandas DataFrame.
-
-    Args:
-        file_path (str): The path to the bedgraph file.
-
-    Returns:
-        Optional[pd.DataFrame]: The DataFrame containing the bedgraph data,
-        or None if an error occurred.
+class BedGraph:
     """
-    try:
-        bedgraph = pd.read_table(file_path, sep="\t")
-        if bedgraph.shape[1] != 4:
-            raise ValueError(f"Bedgraph file at {file_path}"
-                             "does not have exactly 4 columns.")
-        bedgraph.columns = ["CHR", "START", "END", "SCORE"]
-        return bedgraph
-    except (FileNotFoundError, IOError):
-        print(f"{file_path} does not exist or could not be read.")
-        return None
-    except IsADirectoryError:
-        print(f"{file_path} is a directory.")
-        return None
-    except PermissionError:
-        print(f"Permission denied for {file_path}")
-        return None
-    except OSError as e:
-        print(f"OS error occurred: {e}")
-        return None
-
-
-def read_bedbase(file_path: str) -> Optional[pd.DataFrame]:
-    """Reads a bedbase file into a pandas DataFrame. Bedbase files are like
-    bed files but each base is shown instead of genomic windows.
-
-    Args:
-        file_path (str): The path to the bedbase file.
-
-    Returns:
-        Optional[pd.DataFrame]: The DataFrame containing the bedbase data,
-        or None if an error occurred.
+    Represents a BedGraph DataFrame with specific columns: CHR, START, END,
+    SCORE.
     """
-    try:
-        bedbase = pd.read_table(file_path, sep="\t")
-        if bedbase.shape[1] != 3:
-            raise ValueError(f"Bedbase file at {file_path}"
-                             "does not have exactly 3 columns.")
-        bedbase.columns = ["CHR", "BASE", "SCORE"]
-        return bedbase
-    except (FileNotFoundError, IOError):
-        print(f"{file_path} does not exist or could not be read.")
-        return None
-    except IsADirectoryError:
-        print(f"{file_path} is a directory.")
-        return None
-    except PermissionError:
-        print(f"Permission denied for {file_path}")
-        return None
-    except OSError as e:
-        print(f"OS error occurred: {e}")
-        return None
+
+    def __init__(self, CHR, START, END, SCORE):
+        """
+        Initializes a BedBase object.
+
+        Args:
+            CHR (pd.Series): Series representing the chromosome column.
+            START (pd.Series): Series representing start of region.
+            END (pd.Series): Series representing end of region.
+            SCORE (pd.Series): Series representing the score column. Score can
+                be anything, such as number of reads or p-value.
+        """
+        self.df = pd.DataFrame({
+            "CHR": CHR,
+            "START": START,
+            "END": END,
+            "SCORE": SCORE
+        })
+
+    def get(self) -> pd.DataFrame:
+        """
+        Returns underlying pandas DataFrame
+        """
+        return self.df
+
+    @classmethod
+    def read_from_file(cls, file_path: str) -> Optional["BedGraph"]:
+        """Reads a bedgraph file into a pandas DataFrame.
+
+        Args:
+            file_path (str): The path to the bedgraph file.
+
+        Returns:
+            Optional[pd.DataFrame]: The DataFrame containing the bedgraph data,
+            or None if an error occurred.
+        """
+        try:
+            bedgraph = pd.read_table(file_path, sep="\t")
+            if bedgraph.shape[1] != 4:
+                raise ValueError(f"Bedgraph file at {file_path}"
+                                 " does not have exactly 4 columns.")
+            bedgraph.columns = ["CHR", "START", "END", "SCORE"]
+            return cls(
+                bedgraph["CHR"],
+                bedgraph["START"],
+                bedgraph["END"],
+                bedgraph["SCORE"]
+            )
+        except (FileNotFoundError, IOError):
+            print(f"{file_path} does not exist or could not be read.")
+            return None
+        except IsADirectoryError:
+            print(f"{file_path} is a directory.")
+            return None
+        except PermissionError:
+            print(f"Permission denied for {file_path}")
+            return None
+        except OSError as e:
+            print(f"OS error occurred: {e}")
+            return None
 
 
-def read_narrow_peak_file(file_path: str) -> Optional[pd.DataFrame]:
-    """Reads a BED3+7 file from MACS into a pandas DataFrame.
-
-    Args:
-        file_path (str): The path to the bedbase file.
-
-    Returns:
-        Optional[pd.DataFrame]: The DataFrame containing the bed data,
-        or None if an error occurred.
+class BedBase:
     """
-    try:
-        bed = pd.read_table(file_path, sep="\t", skiprows=1)
-        if bed.shape[1] != 10:
-            raise ValueError(f"Bed file at {file_path}"
-                             "does not have exactly 10 columns.")
-        bed.columns = ["CHR", "START", "END", "NAME", "SCORE", "STRAND",
-                       "SIGNAL_VALUE", "PVALUE", "QVALUE", "SUMMIT"]
-        return bed
-    except (FileNotFoundError, IOError):
-        print(f"{file_path} does not exist or could not be read.")
-        return None
-    except IsADirectoryError:
-        print(f"{file_path} is a directory.")
-        return None
-    except PermissionError:
-        print(f"Permission denied for {file_path}")
-        return None
-    except OSError as e:
-        print(f"OS error occurred: {e}")
-        return None
+    Represents a BedBase DataFrame with specific columns: CHR, BASE, SCORE.
+    """
+
+    def __init__(self, CHR, BASE, SCORE):
+        """
+        Initializes a BedBase object.
+
+        Args:
+            CHR (pd.Series): Series representing the chromosome column.
+            BASE (pd.Series): Series representing the base column.
+            SCORE (pd.Series): Series representing the score column. Score can
+                be anything, such as number of reads or p-value.
+        """
+        self.df = pd.DataFrame({
+            "CHR": CHR,
+            "BASE": BASE,
+            "SCORE": SCORE
+        })
+
+    def get(self) -> pd.DataFrame:
+        """
+        Returns underlying pandas DataFrame
+        """
+        return self.df
+
+    @classmethod
+    def read_from_file(cls, file_path: str) -> Optional["BedBase"]:
+        """
+        Reads a bedbase file into a BedBase object.
+
+        Args:
+            file_path (str): The path to the bedbase file.
+
+        Returns:
+            Optional[BedBase]: The BedBase object containing the bedbase data,
+            or None if an error occurred.
+        """
+        try:
+            bedbase = pd.read_table(file_path, sep="\t")
+            if bedbase.shape[1] != 3:
+                raise ValueError(f"Bedbase file at {file_path}"
+                                 " does not have exactly 3 columns.")
+            bedbase.columns = ["CHR", "BASE", "SCORE"]
+            return cls(
+                bedbase["CHR"],
+                bedbase["BASE"],
+                bedbase["SCORE"]
+            )
+        except (FileNotFoundError, IOError):
+            print(f"{file_path} does not exist or could not be read.")
+            return None
+        except IsADirectoryError:
+            print(f"{file_path} is a directory.")
+            return None
+        except PermissionError:
+            print(f"Permission denied for {file_path}")
+            return None
+        except OSError as e:
+            print(f"OS error occurred: {e}")
+            return None
+
+
+class Bed:
+    """
+    Represents a Bed DataFrame with specific columns: CHR, START, END
+    """
+
+    def __init__(self, CHR, START, END):
+        """
+        Initializes a Bed object.
+
+        Args:
+            CHR (pd.Series): Series representing the chromosome column.
+            START (pd.Series): Series representing start of region.
+            END (pd.Series): Series representing end of region.
+        """
+        self.df = pd.DataFrame({
+            "CHR": CHR,
+            "START": START,
+            "END": END
+        })
+
+    def get(self) -> pd.DataFrame:
+        """
+        Returns underlying pandas DataFrame
+        """
+        return self.df
+
+    @classmethod
+    def read_from_file(cls, file_path: str) -> Optional["Bed"]:
+        """Reads a BED3+7 file from MACS into a pandas DataFrame.
+
+        Args:
+            file_path (str): The path to the bedbase file.
+
+        Returns:
+            Optional[pd.DataFrame]: The DataFrame containing the bed data,
+            or None if an error occurred.
+        """
+        try:
+            bed = pd.read_table(file_path, sep="\t", skiprows=1)
+            if bed.shape[1] < 3:
+                raise ValueError(f"Bed file at {file_path}"
+                                 " does not have enough columns.")
+            # Remaining columns that might exist are useless
+            bed = bed.iloc[:, 0:3]
+            bed.columns = ["CHR", "START", "END"]
+            return cls(
+                bed["CHR"],
+                bed["START"],
+                bed["END"]
+            )
+        except (FileNotFoundError, IOError):
+            print(f"{file_path} does not exist or could not be read.")
+            return None
+        except IsADirectoryError:
+            print(f"{file_path} is a directory.")
+            return None
+        except PermissionError:
+            print(f"Permission denied for {file_path}")
+            return None
+        except OSError as e:
+            print(f"OS error occurred: {e}")
+            return None
 
 
 def write_file(data: pd.DataFrame, file_path: str) -> None:
