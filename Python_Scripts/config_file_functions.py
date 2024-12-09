9a596de6393377e719f6a8ea7b62313906482104
sof202 <s.o.fletcher@exeter.ac.uk>
2024-12-09 16:15:40 +0000

    feat: add config file checker for peak compare pipeline
    refactor: split up functions into separate files

---
+++ Python_Scripts/config_file_functions.py
@@ -0,0 +1,110 @@
+import sys
+from pathlib import Path
+
+
+def validate_eol_format(file_path: str) -> None:
+    try:
+        with open(file_path, "rb") as config_file:
+            content: bytes = config_file.read()
+    except IOError:
+        print(
+            "Could not read file",
+            file_path,
+            "Please ensure that this file exists and has read permissions."
+        )
+        sys.exit(1)
+    if b'\r' in content:
+        print(
+            "Carriage return character found in file.",
+            "Please ensure that config file uses Linux EOL characters only."
+        )
+        sys.exit(1)
+
+
+def get_config_variables(file_path: str) -> dict:
+    config_variables: dict = {}
+    with open(file_path, "r") as config_file:
+        for line in config_file:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            variable, value = line.split("=", 1)
+            value = value.strip('"')
+            if value == "":
+                print(
+                    "Could not parse config file.",
+                    f"No value was given for {variable}."
+                )
+                sys.exit(1)
+            config_variables[variable] = value
+    return config_variables
+
+
+def is_variable_missing(variable: str, config_variables: dict) -> bool:
+    if variable not in config_variables:
+        print(
+            f"{variable} is missing from config file.",
+            "Please check the example config for what is required."
+        )
+        return True
+    return False
+
+
+def is_positive_float(x: str, variable_name: str) -> bool:
+    message = f"{variable_name} must be a positive floating point number."
+    try:
+        float(x)
+        if x > 0:
+            return True
+        else:
+            print(message)
+            return False
+    except ValueError:
+        print(message)
+        return False
+
+
+def is_positive_integer(x: str, variable_name: str) -> bool:
+    message = f"{variable_name} must be a positive integer."
+    if not x.isdigit():
+        print(message)
+        return False
+    if int(x) < 0:
+        print(message)
+        return False
+    return True
+
+
+def is_larger(x: str, y: str) -> bool:
+    if not (x.isdigit() and y.isdigit()):
+        print(f"{x} and {y} must be integers.")
+        return False
+    if int(x) >= int(y):
+        print(f"{y} must be strictly greater than {x}")
+        return False
+    return True
+
+
+def path_exists(file_path: str, variable_name: str) -> bool:
+    if Path(file_path).is_file():
+        print(
+            f"The path given for {variable_name} doesn't exist.",
+            f"Please check this path: {file_path}"
+        )
+        return False
+    return True
+
+
+def any_file_paths_missing(config_variables: dict) -> bool:
+    file_missing = False
+    paths_to_check = [
+        "OUTPUT_DIRECTORY",
+        "INPUT_FILE",
+    ]
+    if not is_variable_missing("CONTROL_FILE", config_variables):
+        paths_to_check.append("CONTROL_FILE")
+    for path in paths_to_check:
+        file_missing = file_missing or path_exists(
+            config_variables[path], path
+        )
+    return file_missing
---
+++ Python_Scripts/validate_peak_compare_config.py
@@ -0,0 +1,94 @@
+import argparse
+import sys
+from config_file_functions import (
+    validate_eol_format,
+    get_config_variables,
+    is_positive_integer,
+    is_larger,
+    is_positive_float,
+    is_variable_missing,
+    path_exists
+)
+
+
+def validate_variable_existence(config_variables: dict) -> None:
+    variable_missing = False
+    # Always required
+    variables_to_check = [
+        "CHROMOSOME",
+        "START",
+        "END",
+        "CUTOFF",
+        "REFERENCE_DATASET_OUTPUT_DIRECTORY",
+        "REFERENCE_SAMPLE_NAME",
+        "COMPARISON_DATASET_OUTPUT_DIRECTORY",
+        "COMPARISON_SAMPLE_NAME",
+        "REFERENCE_MERGED_PEAK_FILE",
+        "REFERENCE_UNMERGED_PEAK_FILE",
+        "REFERENCE_BIAS_TRACK_FILE",
+        "REFERENCE_COVERAGE_TRACK_FILE",
+        "COMPARISON_BIAS_TRACK_FILE",
+        "COMPARISON_COVERAGE_TRACK_FILE",
+        "COMPARISON_PVALUE_FILE",
+        "LOG_DIRECTORY"
+    ]
+    for variable in variables_to_check:
+        variable_missing = variable_missing or is_variable_missing(
+            variable, config_variables)
+    # If a required variable is missing, we cannot continue, so exit now.
+    if variable_missing:
+        sys.exit(1)
+
+
+def any_variables_incorrect(config_variables: dict) -> bool:
+    variable_incorrect = False
+    positive_integer_variables = [
+        "START",
+        "END"
+    ]
+    for variable in positive_integer_variables:
+        variable_incorrect = variable_incorrect or is_positive_integer(
+            config_variables[variable],
+            variable
+        )
+    variable_incorrect = variable_incorrect or is_positive_float(
+        config_variables["CUTOFF"],
+        "CUTOFF"
+    )
+    variable_incorrect = variable_incorrect or is_larger(
+        config_variables["START"],
+        config_variables["END"]
+    )
+    return variable_incorrect
+
+
+def any_file_paths_missing(config_variables: dict) -> bool:
+    file_missing = False
+    paths_to_check = [
+        "REFERENCE_DATASET_OUTPUT_DIRECTORY",
+        "COMPARISON_DATASET_OUTPUT_DIRECTORY"
+    ]
+    for path in paths_to_check:
+        file_missing = file_missing or path_exists(
+            config_variables[path], path
+        )
+    return file_missing
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="ChromCompare config file checker",
+        description="Checks whether config file for ChromCompare is valid."
+    )
+    parser.add_argument('file_path')
+    args = parser.parse_args()
+    validate_eol_format(args.file_path)
+    config_variables = get_config_variables(args.file_path)
+    validate_variable_existence(config_variables)
+    config_malformed = False
+    config_malformed = any_variables_incorrect(config_variables)
+    config_malformed = config_malformed or any_file_paths_missing(
+        config_variables)
+    if config_malformed:
+        sys.exit(1)
+    sys.exit(0)
