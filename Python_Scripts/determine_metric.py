eec0cc794477246e5ebd16e5067dd5d7b33fc606
sof202 <s.o.fletcher@exeter.ac.uk>
2024-12-09 14:43:14 +0000

    refactor: pseudopeaks is one word everywhere else
    consistency

--- Python_Scripts/determine_metric.py
+++ Python_Scripts/determine_metric.py
@@ -2,9 +2,9 @@ from IO import BedBase, IncompatabilityError
 
 
 def calculate_metric(reference_labelled_peaks: BedBase,
-                     psuedo_peaks: BedBase,
+                     psuedopeaks: BedBase,
                      include_merged_peaks: bool = True) -> float:
-    if not reference_labelled_peaks.has_same_positions(psuedo_peaks):
+    if not reference_labelled_peaks.has_same_positions(psuedopeaks):
         raise IncompatabilityError(
             "Reference peaks must be over the same region as pseudopeaks.")
     peaks_in_reference = reference_labelled_peaks.get("SCORE")
@@ -13,7 +13,7 @@ def calculate_metric(reference_labelled_peaks: BedBase,
     else:
         number_of_reference_peaks = (peaks_in_reference == 1).sum()
 
-    number_of_pseudopeaks = (psuedo_peaks.get("SCORE") == 1).sum()
+    number_of_pseudopeaks = (psuedopeaks.get("SCORE") == 1).sum()
     metric = number_of_pseudopeaks / number_of_reference_peaks
     if metric > 1:
         print("Peaks in comparison dataset is greater than in reference "
